748a610b999542c61803b005ad290308d710662f
Dmitry Nemkov <xeqlol@gmail.com>
2017-09-14 19:20:23 +0500

    configs and repos

---
+++ HomeCinema.Data/Configurations/EntityBaseConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using HomeCinema.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeCinema.Data.Configurations
+{
+    class EntityBaseConfiguration<T> : EntityTypeConfiguration<T> where T : class, IEntityBase
+    {
+        public EntityBaseConfiguration()
+        {
+            HasKey(e => e.ID);
+        }
+    }
+}
---
+++ HomeCinema.Data/Repositories/EntityBaseRepository.cs
@@ -0,0 +1,50 @@
+﻿using HomeCinema.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeCinema.Data.Repositories
+{
+    class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        private HomeCinemaContext dataContext;
+
+        protected IDbFactory DbFactory
+        {
+            get;
+            private set;
+        }
+
+        protected HomeCinemaContext DbContext
+        {
+            get { return dataContext ?? dataContext = DbFactory.Init(); }
+        }
+
+        public EntityBaseRepository(IDbFactory dbFactory)
+        {
+            DbFactory = dbFactory;
+        }
+
+        public virtual IQueryable<T> GetAll()
+        {
+            return DbContext.Set<T>();
+        }
+
+        public virtual IQueryable<T> All
+        {
+            get { return GetAll(); }
+        }
+
+        public virtual IQueryable<T> AllIncluding(params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = DbContext.Set<T>();
+            foreach(var includeProperty in includeProperties)
+            {
+                query = query.include
+            }
+        }
+    }
+}
